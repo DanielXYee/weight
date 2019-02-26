22cee8be93312c1977752bd14984ce3af2510b7c
陈梓豪 <qc_25f3308ce5ac443886ce50fc2fb6c023@git.code.tencent.com>
2019-02-26 19:05:47 +0800

    update sth

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var createError = require('http-errors');
 var express = require('express');
+var fs = require('fs');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,13 +16,26 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ extended: true,limit:'50mb' }));
 app.use(cookieParser());
+
 app.use('/static',express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.post('/upload',function (req,res) {  //这里参数加上刚刚的解析的而且这里不是get了
+    var dataPath = path.resolve('./public/poses/1.json')
+    var result = req.body
+    try {
+        fs.writeFileSync(dataPath,JSON.stringify(result));
+    }
+    catch (e) {
+        console.log(e);
+    }
+    res.send('ok');
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
