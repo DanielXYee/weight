cc22b4bf0e0978deb4dc3bb1db864fa39b8abb32
sppleHao <53408633@qq.com>
2019-03-28 09:11:32 +0800

    init repo

---
+++ app.js
@@ -0,0 +1,100 @@
+var createError = require('http-errors');
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ extended: true,limit:'50mb' }));
+app.use(cookieParser());
+
+app.use('/static',express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.get('/videoList',function (req,res) {  //这里参数加上刚刚的解析的而且这里不是get了
+    let videos = []
+    const filePath = path.resolve(`./public/videos`)
+    try {
+        fs.readdir(filePath,(err,files)=>{
+            if (err){
+                console.log(err)
+                throw err
+            }
+
+            (function iterator(i){
+                if(i == files.length) {
+                    console.log(videos);
+                    res.send(JSON.stringify(videos));
+                    return;
+                }
+                fs.stat(path.join(filePath, files[i]), function(err, stat){
+                    if(stat.isFile()){
+                        videos.push(files[i]);
+                    }
+                    iterator(i+1);
+                });
+            })(0);
+        })
+    }
+    catch (e) {
+        console.log(e);
+    }
+})
+
+app.post('/upload',function (req,res) {  //这里参数加上刚刚的解析的而且这里不是get了
+    var videoName = req.query.name
+    var dataPath = path.resolve(`./public/poses/${videoName}.json`)
+    var result = req.body
+    try {
+        fs.writeFileSync(dataPath,JSON.stringify(result));
+    }
+    catch (e) {
+        console.log(e);
+    }
+    res.send('ok');
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.use('*',function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*'); //这个表示任意域名都可以访问，这样写不能携带cookie了。
+//res.header('Access-Control-Allow-Origin', 'http://www.baidu.com'); //这样写，只有www.baidu.com 可以访问。
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');//设置方法
+    if (req.method == 'OPTIONS') {
+        res.send(200); // 意思是，在正常的请求之前，会发送一个验证，是否可以请求。
+    }
+    else {
+        next();
+    }
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
